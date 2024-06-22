b0ccb155a986021c506169180b4dc6d7649698da
Adrian Joltea <jolteaadrian@gmail.com>
2024-06-22 13:10:12 +0300

    Remove some unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { useState, useRef, createContext, useEffect } from "react";
-import { useFrame, useThree } from "@react-three/fiber";
+import { useThree } from "@react-three/fiber";
 import ComputerDesk from "./ComputerDesk";
 
 import { Center, Stage } from "@react-three/drei";
 import AdrianDesk from "./AdriansDesk";
 
 function App() {
-	const { viewPort } = useThree();
+  const { viewPort } = useThree();
 
-	return (
-		<>
-			<ambientLight intensity={1} />
-			<Center>
-				<Stage
-					environment={"apartment"}
-					intensity={1}>
-					{/* <ComputerDesk scale={0.8} /> */}
-					<AdrianDesk />
-				</Stage>
-			</Center>
-		</>
-	);
+  return (
+    <>
+      <ambientLight intensity={1} />
+      <Center>
+        <Stage environment={"apartment"} intensity={1}>
+          {/* <ComputerDesk scale={0.8} /> */}
+          <AdrianDesk scale={1.3} />
+        </Stage>
+      </Center>
+    </>
+  );
 }
 
 export default App;
